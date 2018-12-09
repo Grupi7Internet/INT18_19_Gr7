76cd118de1c37c528e7b43d32fca308c4d093e22
elti <gencibalaj@gmail.com>
2018-12-09 15:43:00 +0200

    Javascript

--- scripts/slideshow.js
+++ scripts/slideshow.js
@@ -73,12 +73,7 @@ function SlideImage(x){
      if(current == -1) 
        current=len-1;
 
-$(imgs[current % len]).hide("slide",{direction: to},1000)
-    .attr("data","");
-     $(controls[current % len]).attr("class","slideshow-button");
-     current+=x;
-     if(current == -1) 
-       current=len-1;
+
      $(imgs[current % len]).show("slide",{direction: from},1000).attr("data","current");
      $(controls[current % len]).attr("class","slideshow-button slideshow-button-current");
      $(".slideshow-title").text(array[current%len].caption);
