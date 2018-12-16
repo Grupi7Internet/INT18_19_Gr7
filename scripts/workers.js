8ffae501efde9cb940cc1ccb562b04228e230598
elti <gencibalaj@gmail.com>
2018-12-16 15:43:00 +0200

    Javascript

--- scripts/workers.js
+++ scripts/workers.js
@@ -1,2 +1,12 @@
 var seconds = 0;
 
+function CalculateTime(){
+	seconds++;
+	var date = new Date(null);
+	date.setSeconds(seconds); 
+	var result = date.toISOString().substr(11, 8);
+	postMessage(result);
+	setTimeout("CalculateTime()",1000); 
+}
+
+CalculateTime();
