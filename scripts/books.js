e2fba7702d5e5103bad9efffbec87c8ab9da9151
elti <eltimusa4@gmail.com>
2018-12-25 15:43:00 +0200

    Javascript

---
+++ scripts/books.js
@@ -0,0 +1,216 @@
+
+ function Book(name,description,img,pages,author,rating,audio,pdf){
+        this.name = name;
+        this.description = description; 
+        this.img = img;
+        this.pages = pages;
+        this.author = author;
+        this.rating = rating;
+        this.audio = audio;
+        this.pdf = pdf;
+}
+
+Book.prototype.audioType = "mp3";
+Book.prototype.fileType = "pdf";
+
+
+var Book1 = new Book("City on The Edge",
+        "Anyone who tries to make a distinction between education and entertainment doesn’t know the first thing about either",
+        "images/book1.jpeg",
+        "200",
+        "Eltion Musa",
+        "8.0",
+        "audio/book1.mp3",
+        "files/1.pdf");
+
+
+Book1.ToStr = function(){
+    return this.name +" "+this.author;
+}
+
+var Book2 = new Book("Red Cloks",
+        "Anyone who tries to make a distinction between education and entertainment doesn’t know the first thing about either",
+        "images/book2.jpg",
+        "200",
+        "Eltion Musa",
+        "8.0",
+        "audio/book1.mp3",
+        "files/1.pdf");
+
+var Book3 = new Book("City on The Edge",
+        "Anyone who tries to make a distinction between education and entertainment doesn’t know the first thing about either",
+        "images/book3.jpg",
+        "200",
+        "Eltion Musa",
+        "8.0",
+        "audio/book1.mp3",
+        "files/1.pdf");
+
+var Book4 = new Book("City on The Edge",
+        "Anyone who tries to make a distinction between education and entertainment doesn’t know the first thing about either",
+        "images/book4.jpg",
+        "200",
+        "Eltion Musa",
+        "8.0",
+        "audio/book1.mp3",
+        "files/1.pdf");
+
+var Book5 = new Book("City on The Edge",
+        "Anyone who tries to make a distinction between education and entertainment doesn’t know the first thing about either",
+        "images/book5.jpeg",
+        "200",
+        "Eltion Musa",
+        "8.0",
+        "audio/book1.mp3",
+        "files/1.pdf");
+
+var Book6 = new Book("City on The Edge",
+        "Anyone who tries to make a distinction between education and entertainment doesn’t know the first thing about either",
+        "images/book5.jpeg",
+        "200",
+        "Eltion Musa",
+        "8.0",
+        "audio/book1.mp3",
+        "files/1.pdf");
+
+/*var json = [{
+        "name": "City on The Edge",
+        "description": "Anyone who tries to make a distinction between education and entertainment doesn’t know the first thing about either",
+        "img": "images/book1.jpeg",
+        "pages": "200",
+        "author": "Eltion Musa",
+        "rating": "8.0",
+        "audio": "audio/book1.mp3",
+        "pdf": "files/1.pdf"
+    },
+    {
+        "name": "Red Cloks",
+        "description": "Anyone who tries to make a distinction between education and entertainment doesn’t know the first thing about either",
+        "img": "images/book2.jpg",
+        "pages": "300",
+        "author": "Mark GoodMan",
+        "rating": "10.0",
+        "audio": "audio/book1.mp3",
+        "pdf": "files/1.pdf"
+    },
+    {
+        "name": "Murders",
+        "description": "Anyone who tries to make a distinction between education and entertainment doesn’t know the first thing about either",
+        "img": "images/book3.jpg",
+        "pages": "202",
+        "author": "Leni",
+        "rating": "9.0",
+        "audio": "audio/book1.mp3",
+        "pdf": "files/3.pdf"
+    },
+    {
+        "name": "A ClockWorker",
+        "description": "Anyone who tries to make a distinction between education and entertainment doesn’t know the first thing about either",
+        "img": "images/book4.jpg",
+        "pages": "103",
+        "author": "Anthony Burgess",
+        "rating": "7.5",
+        "audio": "audio/book1.mp3"
+    },
+    {
+        "name": "The Plague",
+        "description": "Anyone who tries to make a distinction between education and entertainment doesn’t know the first thing about either",
+        "img": "images/book5.jpg",
+        "pages": "193",
+        "author": "Albert Camus",
+        "rating": "8.0",
+        "audio": "audio/book1.mp3"
+    },
+    {
+        "name": "Titanic",
+        "description": "Anyone who tries to make a distinction between education and entertainment doesn’t know the first thing about either",
+        "img": "images/book5.jpg",
+        "pages": "195",
+        "author": "Eltion Musa"
+    }
+]*/
+
+
+json = [Book1,Book2,Book3,Book4,Book5,Book6];
+
+var index = 0;
+
+window.onload = function(){
+    AddBooks();    
+    drawStar();
+}
+
+function AddBooks() {
+
+for (var i = 0; i < json.length; i++) {
+        var ul = document.getElementById("list");
+        var li = document.createElement("li");
+        var item = document.createElement("div");
+        var img = document.createElement("img");
+        img.setAttribute("data-index", i);
+        img.addEventListener("click", function() {
+            changeBook(this);
+        });
+        img.src = json[i].img;
+        img.width = 130;
+        img.height = 180;
+        item.setAttribute("class", "grid-item1");
+        item.appendChild(img);
+        li.appendChild(item);
+        ul.appendChild(li);
+        if (i == 0) {
+            changeBook(img);
+        }
+
+    }
+}
+
+function closeFullDiv() {
+    document.getElementById("FullDiv").style.display = "none";
+    document.getElementById("audio").pause();
+}
+
+
+function changeBook(element) {
+    $("#description").hide();
+    index = element.getAttribute("data-index");
+    document.getElementById("poster").src = json[index].img;
+    document.getElementById("book-title").innerHTML = json[index].name;
+    document.getElementById("description-text").innerHTML = json[index].description;
+    document.getElementById("pages").innerHTML = json[index].pages + "    pages";
+    document.getElementById("author").innerHTML = "Author: " + json[index].author;
+    document.getElementById("rating").innerHTML = json[index].rating;
+    $("#description").show("slide", {
+        direction: "down"
+    }, 1000);
+}
+
+function drawStar(){
+
+    var canvas = document.getElementById('star');
+    var ctx = canvas.getContext('2d');
+    ctx.fillStyle = "white";
+    ctx.beginPath();
+    ctx.moveTo(108 / 5, 0.0);
+    ctx.lineTo(141 / 5, 70 / 5);
+    ctx.lineTo(218 / 5, 78.3 / 5);
+    ctx.lineTo(162 / 5, 131 / 5);
+    ctx.lineTo(175 / 5, 205 / 5);
+    ctx.lineTo(108 / 5, 170 / 5);
+    ctx.lineTo(41.2 / 5, 205 / 5);
+    ctx.lineTo(55 / 5, 131 / 5);
+    ctx.lineTo(1 / 5, 78 / 5);
+    ctx.lineTo(75 / 5, 68 / 5);
+    ctx.lineTo(108 / 5, 0);
+    ctx.closePath();
+    ctx.fill();
+}
+
+function playBook() {
+    var audioPlayer = document.getElementById("audio");
+    var pdfLink = document.getElementById("pdf");
+    document.getElementById("FullDiv").style.display = "block";
+    audioPlayer.src = json[index].audio;
+    audioPlayer.play();
+    pdf.href = json[index].pdf;
+}
---
+++ scripts/game.js
@@ -0,0 +1,208 @@
+var rightPressed = false;
+		var leftPressed = false;
+		var topPressed = false;
+		var downPressed = false;
+
+
+
+
+window.onload = function(){
+	var canvas = document.getElementById("Game");
+
+	var img = document.createElement("img");
+	img.src = "images/emoji.png";
+
+	document.addEventListener('keydown',keypressed);
+	document.addEventListener('keyup',keyreleased);
+
+		var ctx = canvas.getContext("2d");
+		var x = canvas.width/2;
+		var y = canvas.height-30;
+
+		var xt = canvas.width/2;
+		var yt = canvas.height-100;
+
+		var radius = 10;
+
+		var dspeed = 2;
+		var tspeedx = 2;
+		var tspeedy = -2;
+		var score = 100;
+		var scoreSpan = document.getElementById("score");
+
+		GetBestScore();
+
+		function drawEmoji(){
+			ctx.drawImage(img,x,y,radius*2,radius*2);
+		}
+
+		function drawTarget(){
+			ctx.beginPath();
+			ctx.strokeStyle = "white";
+			ctx.lineWidth = 4;
+			ctx.arc(xt, yt, 15, 0, Math.PI*2);
+			ctx.stroke();
+			ctx.closePath();
+		}
+
+		function drawText(text){
+			ctx.font = "30px Verdana";
+				var gradient = ctx.createLinearGradient(0, 0, canvas.width, 0);
+				gradient.addColorStop("0"," magenta");
+				gradient.addColorStop("0.5", "blue");
+				gradient.addColorStop("1.0", "red");
+				ctx.fillStyle = gradient;
+				ctx.fillText(text, canvas.width/2, canvas.height/2);
+				clearInterval(ID);
+				setTimeout(function(){
+					location.reload();
+				},2000);
+		}
+
+		function draw() {
+			ctx.clearRect(0, 0, canvas.width, canvas.height);
+    		drawEmoji();
+    		drawTarget();
+
+    		if(xt + tspeedx > canvas.width || xt + tspeedx < 20) {
+			    tspeedx = -tspeedx;
+			}
+			if(yt + tspeedy > canvas.height-20 || yt + tspeedy < 20) {
+			    tspeedy = -tspeedy;
+			}
+
+			xt += tspeedx;
+			yt += tspeedy;
+
+			if(rightPressed && x < canvas.width-2*radius){
+				x += dspeed;
+			}
+			if(leftPressed && x > 0){
+				x -= dspeed;
+			}
+			if(topPressed && y > 0){
+				y -= dspeed;
+			}
+			if(downPressed && y < canvas.height-2*radius){
+				y += dspeed;
+			}
+
+			if((x+2*radius) <= (xt+15) && x >=  (xt-15) && (y+2*radius) <= (yt+15) && y >= (yt-15)){
+				drawText("FITORE!");
+				DrawMan(0,Math.PI,60);
+				UpdateBestScore(score);
+			}
+			radius+=0.003;
+			if(radius > 15){
+				drawText("HUMBJE!");
+			}
+			Score();
+		}
+
+		var ID;
+		img.onload = function(){
+			ID = setInterval(draw, 10);
+		}
+
+		function RelodeGame(){
+			location.reload();			
+		}
+
+		function UpdateBestScore(currentScore){
+			var a;
+			if(isNaN(sessionStorage.score)){
+				a = 0;
+			}else {
+				a = sessionStorage.score;
+			}
+			if(currentScore > a){
+				sessionStorage.score = currentScore;
+			}
+		}
+
+		function GetBestScore(){
+			if(sessionStorage.score != null){
+				$("#BestScore").html("<i>"+Math.round(sessionStorage.score)+"</i>");
+			}else {
+				$("#BestScore").html("<i>0</i>");
+			}
+		}
+
+
+		function Score(){
+			score = 100 - (radius-10)/5*100;
+			scoreSpan.textContent=Math.round(score);
+		}
+
+		DrawMan(Math.PI,Math.PI*2,70);
+		function DrawMan(a,b,c){
+
+			var canvas1 = document.getElementById("canvas1");
+			
+			context = canvas1.getContext("2d"); 
+			context.clearRect(0, 0, canvas1.width, canvas1.height);
+			context.beginPath();
+			context.fillStyle = "#ffe4c4"; // #ffe4c4
+			context.arc(200, 50, 30, 0, Math.PI * 2, true); 
+			
+			context.fill();
+
+			context.beginPath();
+			context.strokeStyle = "red"; 
+			context.lineWidth = 3;
+			context.arc(200, c, 10, a, b, false); 
+			context.stroke();
+
+			// eyes
+			context.beginPath();
+			context.fillStyle = "green";
+			context.arc(190, 45, 3, 0, Math.PI * 2, true); 
+			context.fill();
+			context.arc(210, 45, 3, 0, Math.PI * 2, true); 
+			context.fill();
+
+			// body
+			context.beginPath();
+			context.moveTo(200, 80);
+			context.lineTo(200, 180);
+			context.strokeStyle = "navy";
+			context.stroke();
+
+			// arms
+			context.beginPath();
+			context.strokeStyle = "#0000ff"; // blue
+			context.moveTo(200, 80);
+			context.lineTo(150, 130);
+			context.moveTo(200, 80);
+			context.lineTo(250, 130);
+			context.stroke();
+
+			// legs
+			context.beginPath();
+			context.strokeStyle = "orange";
+			context.moveTo(200, 180);
+			context.lineTo(150, 280);
+			context.moveTo(200, 180);
+			context.lineTo(250, 280);
+			context.stroke();
+		}
+}
+
+
+function keypressed(event){
+	switch(event.keyCode){
+		case 38: topPressed = true;break;
+		case 40: downPressed = true; break;
+		case 37: leftPressed = true;break;
+		case 39: rightPressed = true; break;
+	}
+}
+
+function keyreleased(event){
+	switch(event.keyCode){
+		case 38: topPressed = false;break;
+		case 40: downPressed = false; break;
+		case 37: leftPressed = false;break;
+		case 39: rightPressed = false; break;
+	}
+}
