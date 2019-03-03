65cc572a1fefe97d88aaa3baa270a0e8ae2d7069
Genc Balaj <gencibalaj@gmail.com>
2019-03-03 12:10:00 +0100

    PHP

---
+++ registration.php
@@ -0,0 +1,191 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Register</title>
+
+	<style type="text/css">
+
+	@font-face
+		{
+			font-family: niramit;
+			src:url(fonts/niramit.woff2);
+		}	
+
+	body {
+		background-color: darkslategrey;
+		font-family: niramit; 
+		color: white;
+	}
+
+	form {
+		display: inline-block;
+		text-align: left;
+	}
+
+	input[type="range"] {
+		color: blue;
+	}
+
+	form input[type="text"],input[type="email"],input[type="password"] {
+	background-color: #f2f2f2;
+	font-size: 12pt;
+	padding: 10px 7px 10px 8px;
+	width: 100%;
+	border: none;
+	box-sizing:border-box;
+	margin-bottom: 10px; 
+}
+
+select, input[list] {
+	background-color: #f2f2f2;
+	padding: 10px;
+	border: 1px solid gray;
+	width: 100%;
+	margin-bottom: 10px; 
+	box-sizing:border-box;
+}
+
+form input[type="submit"] {
+	background-color: #f2f2f2;
+	width: inherit;
+	padding: 10px;
+	box-sizing:border-box;
+	margin-bottom: 10px; 
+}
+h1 {
+	font-size: 40pt;
+	color: #c7fffc;
+}
+
+form input[type="date"] {
+	background-color: #f2f2f2;
+	border: 1px solid gray;
+	padding: 10px;
+	width: 100%;
+	box-sizing:border-box;
+	margin-bottom: 10px; 
+}
+
+.error {
+	color: red;
+	font-weight: bold;	
+}
+
+.inputError {
+	border: 1px solid red !important;
+}
+
+.inputError::placeholder {
+}
+	</style>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+	<script src="scripts/formValidator.js"></script>
+</head>
+<body>
+
+
+
+	<div style="width: 400px;text-align: center;margin:0px auto;">
+		<h1>REGISTER</h1>
+		<form id="Form"  method="POST" action="registration.php">
+
+			<input type="text" style="width: 49.4%" name="name" autofocus placeholder="First Name" >
+			
+			<input type="text"style="width: 49.4%" name="lname" placeholder="Last Name"><br />
+			<p class="error" style="display: none;">
+				You must fill the fields;
+			</p>
+			
+	
+			<input type="text" name="email" placeholder="Email" onblur="checkUser(this);" autocomplete="on"><br />
+			<p class="error" style="display: none;">
+				Email format is wrong.
+			</p>
+			<p style="display: none;" class="error" id="registred">Email is already registred</p>
+	
+			<input type="text" name="phone" placeholder="Phone"><br />
+			<p>Birthday:</p>
+			<input type="date" name="bday" placeholder="Birthday"><br />
+			<p class="error" style="display: none;">
+				Birthday is wrong.
+			</p>
+
+			<br/>
+			<input id="pw" type="text" name="password" placeholder="Password" form="Form">
+			<br/>
+			<div style="display: none;" class="error">
+				<p>Password should contain at least:</p>
+				<ul type="circle">
+					<li>1 Uppercase</li>
+					<li>1 Number</li>
+					<li>lentgh 8</li>
+				</ul>
+			</div>
+			<input type="text" placeholder="Confirm Password" name="password1">
+			<p style="display: none;" name="passwordnot"  class="error">Password does not match</p>
+			<p>Gender:</p>
+			<input type="radio" name="gender" value="male"> Male <br />
+			<input type="radio" name="gender" value="female"> Female <br />
+			<p class="error" id="genderError" style="display: none;">You must choose one of the options</p>
+		
+
+			<p>What are you?</p>
+
+			<input type="radio" name="status" value="parent"> Parent <br />
+			<input type="radio" name="status" value="student"> Student <br />
+			<p class="error" style="display: none;">Please check one of opsions</p>
+			
+			<br/>
+			<input type="checkbox" id="term" name="termOfUse" value="termOfUse"> Do you agree with our Term of Use? 
+			<br />
+			<p class="error" style="display: none;">Please check term of use </p>
+			<input type="checkbox" name="emailS"> Do you want to get emails for important updates?
+			<br /><br/>
+			<keygen name="security">
+			<input type="submit" name="submit">
+
+			
+		</form>
+	</div>
+
+	<script>
+		var errors = <?php echo $errorString  ?>;
+
+		for(var i =0; i < errors.length;i++){
+			if(errors[i]=="passwordnot")
+			{
+				$("[name='"+errors[i]+"']").show();
+			}
+			else{
+			$("[name='"+errors[i]+"']").next().next().show();
+			}
+		}
+		var b = <?php echo $all ?>;
+		var a = ["name","gender","lname","email","bday","status","password","phone","termOfUse"];
+		for(var i = 0; i < a.length; i++){
+			if(!errors.includes(a[i])){
+				if(a[i] == "gender" || a[i] == "status" || a[i] == "termOfUse"){
+					$("[value='"+b[i]+"']").prop('checked',true);
+				}else{
+					$("[name='"+a[i]+"']").attr("value",b[i]);
+				}
+			}
+		}
+
+		function checkUser(a){
+			var url = "checkUser.php?email="+a.value;
+			$.getJSON(url, function(result){
+      			if(result['error'] == 1){
+      				$("#registred").show();
+      			}else{
+      				$("#registred").hide();
+      			}
+			});
+		}	
+	</script>
+
+</body>
+</html>
