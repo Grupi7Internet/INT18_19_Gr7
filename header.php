1c4acaec0a59f9b9472398a27bf055b837834e98
ditjon <eltimusa4@gmail.com>
2019-03-30 16:03:00 +0100

    index.php

--- header.php
+++ header.php
@@ -1,7 +1,4 @@
-<?php echo "TEST"; ?>
 
-<div id="fb-root"></div>
-<script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.3"></script>
 <header>
     <div id="top" class="">
         <div class="socialNetworks">
