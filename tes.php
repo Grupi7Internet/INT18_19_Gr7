4b0882f692cd9889734fcc1040154bfefc7b52f9
ditjon <gencibalaj@gmail.com>
2019-04-01 21:30:00 +0200

    test

--- tes.php
+++ tes.php
@@ -3,7 +3,7 @@
 
 
 	function generateRandomString($length = 10) {
-	    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+	   // $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
 	    $charactersLength = strlen($characters);
 	    $randomString = '';
 	    for ($i = 0; $i < $length; $i++) {
@@ -13,6 +13,16 @@
 	}
 
 
+
+	function generateRandomString($length = 10) {
+	   // $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+	    $charactersLength = strlen($characters);
+	    $randomString = '';
+	    for ($i = 0; $i < $length; $i++) {
+	        $randomString .= $characters[rand(0, $charactersLength - 1)];
+	    }
+	    return $randomString;
+	}
 
 
 ?>
