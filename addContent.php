884bcbe5a641619f77c7e87cca8bd182a4136606
genci <gencibalaj@gmail.com>
2019-04-07 22:30:00 +0200

    addContent

--- addContent.php
+++ addContent.php
@@ -95,5 +95,68 @@
 
 		</script>
 
+
+		<script type="text/javascript">
+				
+				function addtodb()
+				{ 
+					var a = document.getElementById("aaa").selectedIndex;
+					var L = document.getElementById("Literature");
+					var V = document.getElementById("Video");
+					var P = document.getElementById("Photo");
+					var C = document.getElementById("Course");
+					var F = document.getElementById("file");
+
+					if(a == 0){
+						L.style.display = 'inline';
+						V.style.display = 'none';
+						P.style.display = 'none';
+						C.style.display = 'none';
+						F.style.display = 'inline';
+					}else if(a == 1){
+						L.style.display = 'none';
+						V.style.display = 'block';
+						C.style.display = 'none';
+						P.style.display = 'none';
+						F.style.display = 'inline';
+					}
+				}
+
+
+
+
+		</script>
+		
+		<script type="text/javascript">
+				
+				function addtodb()
+				{ 
+					var a = document.getElementById("aaa").selectedIndex;
+					var L = document.getElementById("Literature");
+					var V = document.getElementById("Video");
+					var P = document.getElementById("Photo");
+					var C = document.getElementById("Course");
+					var F = document.getElementById("file");
+
+					if(a == 0){
+						L.style.display = 'inline';
+						V.style.display = 'none';
+						P.style.display = 'none';
+						C.style.display = 'none';
+						F.style.display = 'inline';
+					}else if(a == 1){
+						L.style.display = 'none';
+						V.style.display = 'block';
+						C.style.display = 'none';
+						P.style.display = 'none';
+						F.style.display = 'inline';
+					}
+				}
+
+
+
+
+		</script>
+
 </body>
 </html>
