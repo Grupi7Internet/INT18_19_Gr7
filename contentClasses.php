134872a5e2cb7bd840683acf810bca58d1ca997f
genci <dijtonthaqi@hotmail.com>
2019-04-10 17:12:45 +0200

    Classes

--- contentClasses.php
+++ contentClasses.php
@@ -12,6 +12,35 @@
 		}
 	}
 
-	
+	class Book extends items {
+		private $pages;
+		private $author;
+		private $rating;
+		private $description;
+
+
+		public function __construct($img,$title,$source,$pages,$author,$rating,$description){
+			parent::__construct($img,$title,$source);
+			$this->pages = $pages;
+			$this->author = $author;
+			$this->rating = $rating;
+			$this->description = $description;
+		}
+
+		public function __get($filed){
+			if(property_exists($this, $filed)){
+				return $this->$filed;
+			}
+		}
+
+		public function __set($filed, $value){
+			if(property_exists($this, $filed)){
+				$this->$filed = $value;
+			}
+		}
+
+
+	}
+
 
 ?>
