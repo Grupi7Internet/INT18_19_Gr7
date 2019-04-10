00d3576f2e6bee5d6d3d5c436fc2d5ae6c2b0eb6
genci <dijtonthaqi@hotmail.com>
2019-04-10 17:12:45 +0200

    Classes

---
+++ contentClasses.php
@@ -0,0 +1,17 @@
+<?php
+
+	abstract class items {
+		protected $img;
+		protected $title;
+		protected $source;
+
+		public function __construct($img,$title,$source){
+			$this->img = $img;
+			$this->title = $title;
+			$this->source = $source;
+		}
+	}
+
+	
+
+?>
