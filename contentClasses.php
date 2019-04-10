216ec2e1b6c44ef0dc16d3bd247eb3de4d2c11ef
genci <dijtonthaqi@hotmail.com>
2019-04-10 17:12:45 +0200

    Classes

--- contentClasses.php
+++ contentClasses.php
@@ -1,6 +1,4 @@
 <?php
-	require_once("connectDB.php");
-
 	abstract class items {
 		protected $img;
 		protected $title;
@@ -16,6 +14,33 @@
 	}
 
 	class Book extends items {
+		private $pages;
+		private $author;
+		private $rating;
+		private $description;
+
+
+		public function __construct($img,$title,$source,$pages,$author,$rating,$description){
+			parent::__construct($img,$title,$source);
+			$this->pages = $pages;
+			$this->author = $author;
+			$this->rating = $rating;
+			$this->description = $description;
+		}
+
+		public function __get($filed){
+			if(property_exists($this, $filed)){
+				return $this->$filed;
+			}
+		}
+
+		public function __set($filed, $value){
+			if(property_exists($this, $filed)){
+				$this->$filed = $value;
+			}
+		}
+
+		class Book extends items {
 		private $pages;
 		private $author;
 		private $rating;
