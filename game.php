1cf9e1f6b32f67df99fabf2c0ace87fdb9b609b0
xemi <azem.ahmetxhekaj1@gmail.com>
2019-04-17 17:12:45 +0200

    gamee

--- game.php
+++ game.php
@@ -73,7 +73,33 @@
 				<button id="down" onmousedown="downPressed=true;" onmouseup=" downPressed = false;">D</button>
 			</div>	
 		</div>
+		<div style="display: inline-block;vertical-align: middle;">
+			<canvas id="canvas1" width="400px" height="300px" >
+			</canvas>
+		</div>
+		<p><a style="color: white;" href="Score.php">View Scores</a></p>
+
 		
+		
+	</div>
+			<div style="display: inline-block;vertical-align: middle;">
+			<canvas id="canvas1" width="400px" height="300px" >
+			</canvas>
+		</div>
+		<p><a style="color: white;" href="Score.php">View Scores</a></p>
+
+		
+		
+	</div>
+			<div style="display: inline-block;vertical-align: middle;">
+			<canvas id="canvas1" width="400px" height="300px" >
+			</canvas>
+		</div>
+		<p><a style="color: white;" href="Score.php">View Scores</a></p>
+
+		
+		
+	</div>
 
 	<script>
 function allowDrop(ev) {
