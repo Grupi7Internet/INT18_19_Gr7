5662bcf8cf63bcb09a56ab7d6c16c99158e639f5
elti <eltimusa4@gmail.com>
2019-04-20 17:10:00 +0200

    File Write

--- log.php
+++ log.php
@@ -6,6 +6,12 @@
 		private $time;
 		private static $count;
 
+		function __construct(){
+			$this->ip = $_SERVER['REMOTE_ADDR'];
+			$this->info = json_decode(file_get_contents("https://ipinfo.io/{$this->ip}/json"));
+			$this->country = (empty($info->country)) ? "Localhost" : $info->country;
+			$this->time = date("d/m/Y G:i:s");
+		}
 
 		function WriteFile() {
 			try
