770c2327caae2876fde2e02cc30a3327c6a2c292
Xemi <azem.ahmetxhekaj1@gmail.com>
2019-04-26 20:10:00 +0200

    reg add

--- reg.php
+++ reg.php
@@ -6,6 +6,19 @@
 
 		$link = md5($email."genci".date("H"));
 
+		$data = array('TO' => $email, 'LINK' => urlencode("http://localhost/projekti/reg.php?activation=$link&e=$email"));
+
+		$query = "UPDATE infos SET confirmed = '$link' WHERE email = '$email';";
+		mysqli_query($conn, $query);
+
+		$options = array(
+		    'http' => array(
+		        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+		        'method'  => 'POST',
+		        'content' => http_build_query($data)
+		    )
+		);
+		
 		
 	}elseif (isset($_GET["activation"])) {
 		
