7c19a9c399a01addf9a091e8fb298a69e2b98024
Genci <gencibalaj@gmail.com>
2019-05-07 18:25:00 +0200

    on books working

---
+++ books.php
@@ -0,0 +1,19 @@
+﻿<?php 
+     session_start();
+    require("connectDB.php");
+    $loggedin = isUserLogedIn();
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="styles/books.css">
+	<link rel="stylesheet" type="text/css" href="styles/header.css">
+    <link rel="stylesheet" type="text/css" href="styles/footer.css">
+	<link href="https://fonts.googleapis.com/css?family=ZCOOL+XiaoWei" rel="stylesheet"> 
+	<script src="scripts/jquery.min.js"></script>
+    <script src="scripts/jquery-ui.min.js"></script>
+    <script src="scripts/books.js"></script>
+	 <style type="text/css">
+    
