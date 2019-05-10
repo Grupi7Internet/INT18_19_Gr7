a2a27d3bca6522c40423adcb32e4f2afcf8d2f79
Genci <eltimusa4@gmail.com>
2019-05-10 13:15:00 +0200

    score working

--- Score.php
+++ Score.php
@@ -8,6 +8,21 @@
 	}else{
 		ReadFile1();
 	}
+
+	function WriteNewScore(){
+		$f = fopen("score.txt", "a");
+		fwrite($f, sprintf("%s : %.2f\n",$_SESSION['user_id'],$_POST['score']));
+		fclose($f);
+	}
+
+	function ReadFile1(){
+		$f = fopen("score.txt", "r");
+		$txt = fread($f, filesize("score.txt"));
+		$txt = preg_split("#\n#", $txt);
+		$array = array();
+		for($i = 0; $i < sizeof($txt)-1; $i++){
+			$data = preg_split('#\s:\s#', $txt[$i]);
+			$GLOBALS['table'] .= "<tr><td>".$data[0]."</td><td>".$data[1]."</td></tr>";
 		}
 	}
 
