0b8624ed1ade3f4e56ff55a3c2cce3e3ba8160fa
Genci <eltimusa4@gmail.com>
2019-05-10 13:15:00 +0200

    mail

--- mail.php
+++ mail.php
@@ -3,4 +3,8 @@
     $to = $_POST["TO"];
     $link = urldecode($_POST["LINK"]);
 
-    ?>
+    $subject = "Active Account";
+    $message = "Follow the link below to active your account on BackToSchool \n\n" . $link;
+    $headers = "From:" . $from;
+    mail($to,$subject,$message, $headers);
+?>
