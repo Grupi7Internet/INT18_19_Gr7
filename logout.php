a6ce1a337a1279612dff6768c8780cd9ecd29d35
Genci <ditjonthaqi@hotmail.com>
2019-05-20 14:15:00 +0200

    add files

--- logout.php
+++ logout.php
@@ -2,7 +2,11 @@
 	session_start();
 	unset($_SESSION['user_id']);
 	if(isset($_COOKIE['u_token'])){
+		setcookie('u_token', '', time()-24*3600);
+		setcookie('u_email','', time()-24*3600);
 		
+		setcookie('u_token', '', time()-24*3600);
+		setcookie('u_email','', time()-24*3600);
 	}
 	session_destroy();
 	header("Location: index.php");
